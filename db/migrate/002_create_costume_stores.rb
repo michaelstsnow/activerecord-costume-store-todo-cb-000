93a7e4840fec3bf38957d7f043f01067eb75910d
michaelstsnow <michael.st.snow@gmail.com>
2019-09-15 10:08:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,10 @@
 # Create your costume_stores migration here
 class CreateCostumesStores < ActiveRecord::Migration[5.0]
   def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :part_number
+
+    end
   end
 end
