26c730e3fe972afbfdefc3772056e4588c49643d
michaelstsnow <michael.st.snow@gmail.com>
2019-09-15 10:08:27 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,5 +9,11 @@
 # value types according to the spec
 class CreateCostumes < ActiveRecord::Migration[5.0]
   def change
+    create_table :products do |t|
+      t.string :name
+      t.string :part_number
+
+      t.timestamps
+    end
   end
 end
