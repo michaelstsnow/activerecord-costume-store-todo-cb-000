103f82722f1869592e90c069c0316766eb72cd74
michaelstsnow <michael.st.snow@gmail.com>
2019-09-15 10:09:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,7 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.0]
   def change
-    create_table :costumes do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :part_number
 
